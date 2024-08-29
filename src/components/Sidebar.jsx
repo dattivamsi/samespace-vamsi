9cf8a79e00373015f26a06d34ea534e33618dedf
dattivamsi <dattitarun@gmail.com>
2024-08-29 19:05:22 +0530

    css changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar = ({ logo, profile }) => {
   return (
     <div className="sidebar">
       <div>
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className="logo-img"/>
       </div>
       <div>
         <img src={profile} alt="profile" />
